77eaad533b11516c653d1435297c98d57a50887a
ssurgutsky <ssurgutsky@gmail.com>
2020-04-04 13:24:30 +0700

    - update from sg

--- src/components/model/GameModel.vue
+++ src/components/model/GameModel.vue
@@ -61,12 +61,12 @@ export default {
       this.createMarksDictionary(scenario)
       // console.log(this.marksDictionary)
 
-      console.log('gameScriptsDictionary:', this.gameScriptsDictionary)
-
-      commonUtils.runTests()
-      this.runTests()
-
       if (process.env.$debug) {
+        console.log('gameScriptsDictionary:', this.gameScriptsDictionary)
+
+        commonUtils.runTests()
+        this.runTests()
+
         this.initStore()
       } else {
         document.addEventListener('deviceready', this.initStore)
@@ -81,19 +81,21 @@ export default {
         return
       }
 
+      // window.store.verbosity = window.store.DEBUG
+
       window.store.register({
-        id: 'com.kri_games.sg5.coin',
-        alias: 'coin',
+        id: 'com.kri_games.ma1.coin1',
+        alias: 'coin1',
         type: window.store.CONSUMABLE
       })
 
+      // Store global error handler (to see errors in console in Chrome inspect)
       window.store.error(function (error) {
         console.log('%c ERROR ' + error.code + ': ' + error.message, 'background: #FF0000; color: #FFFFFF')
       })
 
-      // ... MORE HERE SOON
-
       window.store.refresh()
+
       this.gameData.store = window.store
     },
 
@@ -102,8 +104,20 @@ export default {
       if (markName) {
         console.log('Saving... markName:', markName)
         localStorage.setItem('markName', markName)
+
+        // Temporary remove IAP store from gameData but remember IAP store link
+        let storeLink = this.gameData.store
+        this.gameData.store = null
+
+        // Remove Vue's reactivity
         const saveObj = { ...this.gameData }
+
+        // Save the copy to local storage
         localStorage.setItem('gameData', JSON.stringify(saveObj))
+
+        // Restore link to the IAP store
+        this.gameData.store = storeLink
+
         console.log('gameData:', saveObj)
         return true
       }
@@ -114,7 +128,15 @@ export default {
       let markName = localStorage.getItem('markName')
       console.log('Loading... markName:', markName)
       if (markName) {
+        // Remember link to the IAP store
+        let storeLink = this.gameData.store
+
+        // Load data from the local storage
         this.gameData = JSON.parse(localStorage.getItem('gameData'))
+
+        // Restore link to the IAP store
+        this.gameData.store = storeLink
+
         console.log('gameData:', this.gameData)
         this.gotoMark(markName)
         return true
