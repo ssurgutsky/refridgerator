7b7e40cf3d8396e5d8cff54f4db6b6996860c931
ssurgutsky <ssurgutsky@gmail.com>
2020-07-16 14:19:00 +0700

    - indexeddb

---
+++ src/components/Settings.js
@@ -0,0 +1,9 @@
+/* eslint no-eval: 0 */
+
+export default {
+  INDEXEDDB_VERSION: 5,
+  INDEXEDDB_STORE_NAME: 'store_fridge',
+  CACHE_ENABLED: true,
+  GP_PRODUCT1_NAME: 'com.skystairgames.fridge.coin1',
+  GP_PRODUCT2_NAME: 'com.skystairgames.fridge.episodes'
+}
